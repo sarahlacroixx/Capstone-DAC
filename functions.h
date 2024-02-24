a8722974f36159c0180d97a867ae2bc9911b1205
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-02-24 13:36:13 -0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,1 +1,15 @@
 //place headers here
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <Arduino.h>
+
+void stepperMotorsClose();
+void lcdDisplay(int currentTemp, int currentCO2);
+void lcdDisplayStarting();
+void startFans();
+int checkCO2();
+int checkTempElecBox();
+int checkTherms();
+
+#endif
