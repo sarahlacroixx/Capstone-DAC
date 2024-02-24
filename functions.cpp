96c03dae0be90dac210585f6bf1ad8a7bdc5d250
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-02-24 14:24:18 -0500

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -25,9 +25,42 @@ void lcdDisplayStarting(){
 }
 
 void startFans(){
+  digitalWrite(fan1, HIGH);
+  digitalWrite(fan2, HIGH);
+  
+}
+void turnOffFans(){
+  digitalWrite(fan1, LOW);
+  digitalWrite(fan2, LOW);
 }
 
 int checkCO2(){
+  // start I2C
+
+  Wire.beginTransmission(ADDR_6713);
+  Wire.write(0x04);
+  Wire.write(0x13);
+  Wire.write(0x8B);
+  Wire.write(0x00);
+  Wire.write(0x01);
+  // end transmission
+  Wire.endTransmission();
+  // read report of current gas measurement in ppm
+  delay(2000);
+  Wire.requestFrom(ADDR_6713, 4); //request 4 bytes from slave device
+
+  data[0] = Wire.read();
+  data[1] = Wire.read();
+  data[2] = Wire.read();
+  data[3] = Wire.read();
+
+  Serial.print("Func code: "); Serial.print(data[0],HEX);
+  Serial.print(" byte count: "); Serial.println(data[1],HEX);
+  Serial.print("MSB: 0x");  Serial.print(data[2],HEX);
+  Serial.print("  ");
+  Serial.print("LSB: 0x");  Serial.print(data[3],HEX);
+  Serial.print("  ");
+  CO2ppmValue = ((data[2] * 0xFF ) + data[3]);
 }
 
 int checkTempElecBox() {
