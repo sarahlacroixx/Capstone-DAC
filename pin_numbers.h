794236065c567baee373fd05cf57d0c27c6dfdce
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-02-27 15:20:26 -0500

    Update pin_numbers.h

--- pin_numbers.h
+++ pin_numbers.h
@@ -4,4 +4,33 @@
 //Define pin numbers in this file to keep main cleaner
 //use format #DEFINE pinName Pin# 
 
+//buttons and switch
+#DEFINE startButton 0
+#DEFINE stopButton 0
+#DEFINE limitSwitch 0
+
+//lcd display
+#DEFINE ledColumns 16
+#DEFINE ledRows 2
+
+//stepper motor pins
+#DEFINE DIR1  0
+#DEFINE STEP1  0
+#DEFINE DIR2  0
+#DEFINE STEP2  0
+#DEFINE motorInterfacetype 1
+
+//relay heat pins
+
+//fan pins
+
+//thermisters
+
+//pump
+
+//relay solenoid pins
+
+
+
+
 #endif
