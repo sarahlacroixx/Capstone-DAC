4b95bb62e04865aa58f0e56135f46bf5b91e2f7b
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-02-27 15:07:38 -0500

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -3,7 +3,7 @@
 
 //CO2 things
 int data [4];
-int CO2ppmValue;
+double CO2ppmValue;
 
 void stepperMotorsClose(){
     //change pin number depending on limit switch pin
@@ -44,33 +44,6 @@ void turnOffFans(){
 }
 
 double checkCO2(){
-  // start I2C
-
-  Wire.beginTransmission(ADDR_6713);
-  Wire.write(0x04);
-  Wire.write(0x13);
-  Wire.write(0x8B);
-  Wire.write(0x00);
-  Wire.write(0x01);
-  // end transmission
-  Wire.endTransmission();
-  // read report of current gas measurement in ppm
-  delay(2000);
-  Wire.requestFrom(ADDR_6713, 4); //request 4 bytes from slave device
-
-  data[0] = Wire.read();
-  data[1] = Wire.read();
-  data[2] = Wire.read();
-  data[3] = Wire.read();
-
-  Serial.print("Func code: "); Serial.print(data[0],HEX);
-  Serial.print(" byte count: "); Serial.println(data[1],HEX);
-  Serial.print("MSB: 0x");  Serial.print(data[2],HEX);
-  Serial.print("  ");
-  Serial.print("LSB: 0x");  Serial.print(data[3],HEX);
-  Serial.print("  ");
-  CO2ppmValue = ((data[2] * 0xFF ) + data[3]);
-    return CO2ppmValue;
 }
 
 double checkCO2inTank(){
