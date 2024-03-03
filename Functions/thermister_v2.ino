a6021ad083ba05ecf48052a68da437d21dd83582
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-03-02 20:51:21 -0500

    Update thermister_v2.ino

--- Functions/thermister_v2.ino
+++ Functions/thermister_v2.ino
@@ -1,5 +1,6 @@
 float thermistor() {
   const int8_t baseTemp = 25;
+  const int8_t beta = 3984;
   const int8_t numReadings = 4; //The number of readings to average
   const int16_t r0 = 10000; //Thermistor resistance value at 25 degrees Celsius
   const int16_t r1 = 10000; //Fixed Resistor value in the voltage divider circuit
@@ -25,12 +26,13 @@ float thermistor() {
   if(val[i] == 0){
     val[i] = 1;
   }
+  reading = val[i]*f/2;
     
   //Voltage divider equation to solve for thermistor resistance
-  rt = r1 * (4095.00/val[i]-1.00);
+  rt = r1 * (4095.00/reading-1.00);
     
   //Equation to convert thermistor resistance to temperature
-  temp = (1/((1/(273.15+baseTemp))+(1/_beta) * log(rt/r0)))-273.15;
+  temp = (1/((1/(273.15+baseTemp))+(1/beta) * log(rt/r0)))-273.15;
   sum += temp;
   }
   return sum/numReadings;
