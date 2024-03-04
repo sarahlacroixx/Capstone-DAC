6316ee5a0c673612ffa27c413e22c7ea0d79ec48
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-03-03 20:28:28 -0500

    Update functions.h

--- Headers/functions.h
+++ Headers/functions.h
@@ -4,16 +4,27 @@
 #define FUNCTIONS_H
 #include <Arduino.h>
 
-void stepperMotorsClose();
-void lcdDisplay(int currentTemp, int currentCO2);
-void lcdDisplayStarting();
-void startFans();
-void turnOffFans();
-int checkCO2();
-int checkTempElecBox();
-int checkTherms();
-int checkTherms();
-void openValve();
-void turnPumpOn();
+void stepperMotorsClose(AccelStepper motor1, AccelStepper motor2);
+void stepperMotorsOpen(AccelStepper motor1, AccelStepper motor2);
+void lcdDisplayAdsorption(int currentTemp, int currentCO2);
+void lcdDisplayDesorption(int currentTemp, int currentCO2);
+void lcdDisplayWaiting();
+void lcdDisplayCooldown(int currentTemp, int currentCO2);
+void lcdDisplayCapCO2(int finalCO2);
+void startFans(fan1, fan2);
+void turnOffFans(fan1 fan2);
+double checkCO2(SparkFun_ENS160 sensor);
+double checkTempElecBox(TMP102 sensor0);
+void openValve(int valve);
+void closeValve(int valve);
+void startPump(int pump);
+void stopPump(int pump);
+void heatON(int relay);
+void hearOFF(int relay);
+void adsorption(AccelStepper motor1, AccelStepper motor2, int fan1, int fan2, ADS1115 ADS, SparkFun_ENS160 co2Sensor1);
+float checkTherms(ADS1115 ADS);
+void heating(ADS1115 ADS, SparkFun_ENS160 co2Sensor2);
+void desorption(int valve, int pump, ADS1115 ADS, SparkFun_ENS160 co2sensor2);
+void coolDown(AccelStepper motor1, AccelStepper motor2, int fan1, int fan2, ADS1115 ADS, SparkFun_ENS160 co2sensor2);
 
 #endif
