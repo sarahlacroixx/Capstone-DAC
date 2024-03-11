ea8ce82218a74889f004b1aa0be157cb50ed77bf
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-03-11 14:38:50 -0400

    Update functionsSlave.cpp

--- Functions/slave/functionsSlave.cpp
+++ Functions/slave/functionsSlave.cpp
@@ -1,1 +1,20 @@
+//need to load libraries
+//need to create a loop where it just checks the data received
+//include functions from espNOW
 
+void loop(){
+  //retrieve data structure
+  if 
+}
+
+
+//function to start fans
+void startFans(){
+  digitalWrite(fan1, HIGH);
+  digitalWrite(fan2, HIGH);
+}
+
+void stopFans(){
+  digitalWrite(fan1, HIGH);
+  digitalWrite(fan2, HIGH);
+}
