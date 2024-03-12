145cb376929ed4c1adde46bc96aac0b376003e8a
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-03-12 01:06:18 -0400

    Update pin_numbers.h

--- Headers/pin_numbers.h
+++ Headers/pin_numbers.h
@@ -13,27 +13,6 @@
 #DEFINE ledColumns 20
 #DEFINE ledRows 4
 
-//stepper motor pins
-#DEFINE DIR1  28
-#DEFINE STEP1  27
-#DEFINE DIR2  26
-#DEFINE STEP2  25
-#DEFINE DIR3  24
-#DEFINE STEP3  23
-#DEFINE DIR4  20
-#DEFINE STEP4  19
-#DEFINE motorInterfacetype 1
-
-//relay heat pins
-
-//fan pins
-#DEFINE fan1 10
-#DEFINE fan2 11
-
-//pump
-
-//relay solenoid pins
-
 
 
 
