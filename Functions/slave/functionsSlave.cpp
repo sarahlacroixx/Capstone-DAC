612e2445aa0e7a113144291f5e7cff974f6efca0
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-03-11 23:26:37 -0400

    Update functionsSlave.cpp

--- Functions/slave/functionsSlave.cpp
+++ Functions/slave/functionsSlave.cpp
@@ -1,12 +1,84 @@
 //need to load libraries
+
+#include <esp_now.h>
+#include <WiFi.h>
 //need to create a loop where it just checks the data received
 //include functions from espNOW
+typedef struct parameters {
+    bool heat;
+    bool fans;
+    bool pump;
+    bool valve;
 
-void loop(){
-  //retrieve data structure
-  if 
+} parameters;
+
+parameter highVolt;
+
+//FUNCTION FROM LIBRARY
+//everything should happen in this function
+void OnDataRecv(const uint8_t * mac, const uint8_t *recData, int len) {
+  memcpy(&highVolt, recData, sizeof(highVolt));
+  checkFans();
+  checkHeat();
+  checkPump();
+  checkValve();
 }
 
+void setup(){
+  Serial.begin(115200);
+  
+  // Set wifi up
+  WiFi.mode(WIFI_STA);
+
+   // init espnow
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error with init");
+    return;
+  }
+  // callback function
+  esp_now_register_recv_cb(OnDataRecv);
+
+  
+}
+
+void loop(){
+  //chill and wait for data to be received
+}
+void checkFans(){
+  if (highVolt.fans == true){
+    startFans();
+  }
+  else{
+    stopFans();
+  }
+}
+
+void checkHeat(){
+  if (highVolt.heat == true){
+    heatON();
+  }
+  else{
+    heatOFF();
+  }
+}
+
+void checkPump(){
+  if (highVolt.pump == true){
+    startPump();
+  }
+  else{
+    stopPump();
+  }
+}
+
+void checkValve(){
+  if (highVolt.valve == true){
+    openValve();
+  }
+  else{
+    closeValve();
+  }
+}
 
 //function to start fans
 void startFans(){
@@ -17,4 +89,28 @@ void startFans(){
 void stopFans(){
   digitalWrite(fan1, HIGH);
   digitalWrite(fan2, HIGH);
+}
+
+void openValve(int valve){
+  digitalWrite(valve, HIGH);
+}
+
+void closeValve(int valve){
+  digitalWrite(valve, LOW);
+}
+
+void startPump(int pump){
+  digitalWrite(pump, HIGH);
+}
+
+void stopPump(int pump){
+  digitalWrite(pump, LOW);
+}
+
+void heatON(int relay){
+    digitalWrite(relay, LOW);
+}
+
+void hearOFF(int relay){
+    digitalWrite(relay, LOW);
 }
