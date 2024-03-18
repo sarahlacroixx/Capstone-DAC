d041ed5043ffc41d40cfdc97d108339a2a188112
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-03-18 14:38:45 -0400

    Update mainFunc.ino

--- Functions/individualFunc/mainFunc.ino
+++ Functions/individualFunc/mainFunc.ino
@@ -1,86 +1,86 @@
-void adsorption(AccelStepper motor1, AccelStepper motor2, int fan1, int fan2, ADS1115 ADS, SparkFun_ENS160 co2Sensor1){
-    float inCo2 = readCO2(ADS, 0); // NEED TO FIX CHANNEL HERE
-    float outCo2 = readCO2(ADS, 1); // NEED TO FIX CHANNEL HERE
-    float currentTemp = checkTherms(ADS);
-    float elecTemp = 
-    double prevCO2 = 0;
+
+typedef struct currParameters{
+    float inCo2;
+    float outCo2;
+    float currentTemp;
+    float elecTemp;
+    int flowCo2;
+} currParameters;
+
+currParameters curr;
+
+currParameters checkParameters(ADS1115 ADS, TMP102 elecSensor){
+    curr.inCo2 = readCO2(ADS, 0); // NEED TO FIX CHANNEL HERE
+    curr.outCo2 = readCO2(ADS, 1); // NEED TO FIX CHANNEL HERE
+    curr.currentTemp = checkTherms(ADS);
+    curr.elecTemp = checkTempelecBox(elecSensor);
+    curr.flowCo2 = readFlowCO2();
+  
+}
+
+void adsorption(ADS1115 ADS, TMP102 elecSensor){
+    checkParameters(ADS, elecSensor);
+
     //start fans
-    sendData(false, true, false, false);
+    sendData(0, false, true, false, false, 0);
     //wait for 150 minutes to go by
     while (timerFlag = false) {
-        lcdDisplayWithPot("Adsorption", currentTemp, int elecTemp, int inCo2, int outCo2, int flowCo2
-
-        inCo2 = readCO2(ADS, 0); // NEED TO FIX CHANNEL HERE
-        outCo2 = readCO2(ADS, 1); // NEED TO FIX CHANNEL HERE
-        currentTemp = checkTherms(ADS);
-    
+        //flow co2 is 0 as it is not applicable in adsorption phase
+        lcdDisplayWithPot("Adsorption", curr.currentTemp, curr.elecTemp, curr.inCo2, curr.outCo2, 0);
+        checkParameters(ADS, elecSensor);
     }
     //set timer back to false for next cycle
     timerFlag = false;
-    //turn off fans
-    sendData(false, true, false, false);
-    //FIX CODE HERE TO CLOSE STEPPER MOTORS
-    stepperMotorsClose(motor1, motor2);
-        
+    //turn off fans and close motors
+    sendData(0, false, false, false, false, 1);
     
-}
+}      
 
-void checkParameters(){
-
-  }
-          
-
-void heating(ADS1115 ADS, SparkFun_ENS160 co2Sensor2){
-    float currentTemp = checkTherms(ADS);
-    double CO2 = checkCO2(co2Sensor2);
+void heating(ADS1115 ADS, TMP102 elecSensor){
+    checkParameters(ADS, elecSensor);
     
     //turn the heat on here
-    sendData(true, false, false, false);
-    while (currentTemp < 125) {
-        lcdDisplayDesorption(currentTemp, CO2);
-        currentTemp = checkTherms(ADS);
-        CO2 = checkCO2(co2Sensor1);
+    sendData(0, true, false, false, false, 0);
+    while (curr.currentTemp < 125) {
+        lcdDisplayWithPot("Adsorption", curr.currentTemp, curr.elecTemp, curr.inCo2, curr.outCo2, 0);
+        checkParameters(ADS, elecSensor);
     }
 }
 
-void desorption(int valve, int pump, ADS1115 ADS, SparkFun_ENS160 co2sensor2){
+void desorption(ADS1115 ADS, TMP102 elecSensor)){
     heating(ADS, co2Sensor2);
     //starting the pump and valve
-    sendData(true, false, true, true);
-    float currentTemp = checkTherms(ADS);
-    double outputtedCO2 = checkCO2(co2sensor2);
-    double prevCO2 = 0;
+    sendData(0, true, false, true, true, 0);
+    checkParameters(ADS, elecSensor);
     
     //hold until CO2 in the tank has stopped increasing
+    //NEED TO CHANGE THIS WHILE STATEMENT 
     while (outputtedCO2 > prevCO2)  {
         if (currentTemp < 120) {
-            sendData(true, false, true, true);
+            sendData(0, true, false, true, true, 0);
         }
         if (currentTemp > 130) {
-            sendData(false, false, true, true);
+            sendData(0, false, false, true, true, 0);
         }
         prevCO2 = outputtedCO2;
-        outputtedCO2 = checkCO2(co2sensor2);
-        currentTemp = checkTherms(ADS);
-        lcdDisplayDesorption(currentTemp, outputtedCO2);
+        checkParameters(ADS, elecSensor);
+        lcdDisplayWithPot("Desorption", curr.currentTemp, curr.elecTemp, curr.inCo2, curr.outCo2, curr.flowCo2);
     }
-    //turn off heat, pump and valve
-    sendData(false, false, false, false);
+    //turn off heat, pump and valve and open motors
+    sendData(0, false, false, false, false, 1);
 }
 
-void coolDown(AccelStepper motor1, AccelStepper motor2, int fan1, int fan2, ADS1115 ADS, SparkFun_ENS160 co2sensor2){
-    //openStepperMotors(motor1, motor2);
-    float currentTemp = checkTherms(ADS);
-    double finalCO2 = checkCO2(co2sensor2);
+void coolDown(ADS1115 ADS, TMP102 elecSensor){
+    checkParameters(ADS, elecSensor);
     //turn on the fans
-    sendData(false, true, false, false);
-    while (currentTemp > 30) {
-        //startFans(fan1, fan2);
-        lcdDisplayCooldown(currentTemp, finalCO2);
+    sendData(0, false, true, false, false, 0);
+    while (curr.currentTemp > 30) {
+        lcdDisplayWithPot("Desorption", curr.currentTemp, curr.elecTemp, curr.inCo2, curr.outCo2, curr.flowCo2);
+        checkParameters(ADS, elecSensor);
     }
     
     //turn off fans
-    sendData(false, false, false, false);
-    lcdDisplayCapCO2(finalCO2);
+    sendData(0, false, false, false, false, 0);
+    //NEED TO CREATE AN END FUNCTION DISPLAY
     delay(200);
 }
