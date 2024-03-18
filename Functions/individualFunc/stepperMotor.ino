6261044a906e67286fe70f5a519431342d3fcca2
sarahlacroixx <103224566+sarahlacroixx@users.noreply.github.com>
2024-03-18 12:46:54 -0400

    Update stepperMotor.ino

--- Functions/individualFunc/stepperMotor.ino
+++ Functions/individualFunc/stepperMotor.ino
@@ -1,10 +1,19 @@
 
-void runMotorsSet(int ls1, int ls2){
-  bool ls = false;
-  while (ls == false){
+void runMotorsSet(int ls1, int ls2)
+  while (ls1 != 1 && ls2 != 1){
     //can change this to runSpeed if want to control speed of motor
     motor1.run();
     motor2.run();
-    ls = checkLS();
+    ls1 = checkLS(ls1);
+    ls2 = checkLS(ls2);
+  }
+
+}
+
+int checkLS1(int ls){
+  if (digtalRead(ls1) == 1){
+    return 1;
+  }else {
+    return 0;
   }
 }
